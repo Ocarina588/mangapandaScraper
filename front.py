d0c68f7baedcfc0d788262e17a4b67b4e5a5db14
manuel <manuel.medina-marais@epitech.eu>
2020-08-02 15:11:23 +0200

    [+] adding image of the manga

--- front.py
+++ front.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import requests
+from PIL import Image, ImageTk
 import back as B
 
 def enable_scene_1(a):
@@ -35,10 +37,8 @@ def set_button_find():
     button_find.place(x=x, y=y)
 
 def set_manga_name(name):
-    x = WIDTH/2
-    y = HEIGHT/2
     manga_name.config(text=name)
-    manga_name.place(x=x, y=y)
+    manga_name.pack()
 
 def main():
     enable_scene_1(None)
@@ -51,6 +51,19 @@ def find_manga():
     else:
         disable_scene_1()
         set_manga_name(B.get_manga_name(source))
+        temp = open("temp.jpg", "wb")
+        temp.truncate(0)
+        img_url = source.find('div', id='mangaimg').find('img')['src']
+        img_data = requests.get(img_url)
+        temp.write(img_data.content)
+        temp.close()
+        img = Image.open('temp.jpg')
+        photo_manga = ImageTk.PhotoImage(img)
+        photo_manga_label.config(image=photo_manga)
+        photo_manga_label.image=photo_manga
+        photo_manga_label.pack()
+
+
 
 WIDTH = 540
 HEIGHT = 360
@@ -63,13 +76,19 @@ window.minsize(WIDTH, HEIGHT)
 window.maxsize(WIDTH, HEIGHT)
 window.config(background='#1BF186')
 
+# creating temp file
+temp = open("temp.jpg", "w")
+temp.close()
+
 # creating photos
 photo_mangapanda = PhotoImage(file='mangapanda.png')
+photo_manga = PhotoImage()
 
 # creating labels
 photo_mangapanda_label = Label(window, image=photo_mangapanda)
+photo_manga_label = Label(window, imgage=None)
 error = Label(window, text='NOT FOUND', bg='#1BF186', fg='red', font='Arial 18 bold')
-manga_name = Label(window, text='')
+manga_name = Label(window, bg='#1BF186', text='', font='Arial 25 bold')
 
 # creating inputs
 input_url = Entry(window, width=50)
